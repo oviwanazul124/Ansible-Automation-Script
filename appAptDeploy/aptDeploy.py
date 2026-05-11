763758a59c4d59c901d5c3db6b9b08891e7c0809
Javier García <oviwanazuldev@gmail.com>
2026-05-11 20:01:20 +0200

    Hotfix: Funcionamiento correcto?

--- appAptDeploy/aptDeploy.py
+++ appAptDeploy/aptDeploy.py
@@ -26,7 +26,7 @@ def aptDeploy(packg):
     checkPermission(inv)
     aptPlaybook = os.path.join("playbooks", "AppInstall.yml")
     remote_user = configGet('users', 'remote_user')
-    packages = json.dumps({"packages": packg})
+    packages = ",".joiun(packg)
 
     # Ansible playbook to install the apps
 
@@ -35,7 +35,7 @@ def aptDeploy(packg):
         "-i", inv,
         aptPlaybook,
         "-u", remote_user,
-        "-e", packages,
+        "-e", f"packages_list={packages}",
         "-k",
         "-K",
         "--ssh-common-args=-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
