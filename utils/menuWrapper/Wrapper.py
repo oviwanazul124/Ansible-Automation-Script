c361fb250d585ba880a2466397849135d507fc2a
Javier García <oviwanazuldev@gmail.com>
2026-05-12 17:03:42 +0200

    Hotfix: Dependencies Installer

---
+++ utils/menuWrapper/Wrapper.py
@@ -0,0 +1,33 @@
+import subprocess
+from ansible_menus import *
+import os
+
+def clear():
+    subprocess.run(["clear" if os.name == "posix" else "cls"], shell=True)
+
+
+def menuWrapper(title, menuDict):
+    while True:
+        clear()
+        print(f'--- {title} ---')
+        for key, value in menuDict.items():
+            print(f'{key}. {value['label']}')
+        
+        opt = input("\nSelect an option: ")
+
+        if opt in menuDict:
+            item = menuDict[opt]
+
+            if item['label'] in ['Exit', 'Back to Main'] or item['func'] is None:
+                break
+
+            if item['func'] == 'debugMenu':
+                menuWrapper("Debug Menu", debugMenu)
+            elif item['func'] == 'confMenu':
+                menuWrapper("Configuration Menu", confMenu)
+            else:
+                item['func']()
+                input("\nPress Enter to continue...")
+        else:
+            print("Invalid option.Please try again.")
+            input()
